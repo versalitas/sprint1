7b1187304f3d1cb604eba10ef3701d4fec78cef6
MK <caprichismo@gmail.com>
2022-04-29 11:07:43 +0200

    add manual mock and test of person class, untangle json mess,

--- sprint1_6/app/person.js
+++ sprint1_6/app/person.js
@@ -1,10 +1,18 @@
 
 
-module.exports = class Person{
+//original class to be tested 
+
+class Person {
   constructor(name){
       this.name = name;
   }
   sayName(){
-      console.log(this.name);
+      console.log(`My name is ${this.name}.`);
   }
 }
+ 
+
+
+module.exports = Person;
+
+
